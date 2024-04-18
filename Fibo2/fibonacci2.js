ab3899fdf676a73dcde7fd99c4dd5b931507b7fd
Emiliano Molina <emolina@w3itsolutions.net>
2024-04-18 12:04:00 -0300

    wip second excercise

--- Fibo2/fibonacci2.js
+++ Fibo2/fibonacci2.js
@@ -1,8 +1,8 @@
 // Partiendo del ejercicio Fibonacci,
 
 //1- Copiarlo en otra carpeta llamada Fibo2                                   [DONE]
-//2- Orientación: horizontal (h) o vertical (v)                               []
-//3- Dirección: directa (d) o inversa (i)                                     []
+//2- Orientación: horizontal (h) o vertical (v)                               [DONE]
+//3- Dirección: directa (d) o inversa (i)                                     [DONE]
 //4- Si es especifica una opción no válida debe indicarse "Opción no válida". []
 //5- El programa puede invocarse sin la opción "-o" en cuyo caso el comportamiento debe ser por default hd.
 
@@ -39,9 +39,8 @@
 // java -jar fibo.jar 8
 // fibo<8>: 0 1 1 2 3 5 8 13
 
-const valor = process.argv[4];
+const valor = process.argv[3];
 const orientacion = process.argv[2];
-const direccion = process.argv[3];
 
 let a = 0;
 let b = 1;
@@ -56,29 +55,26 @@ for (let i = 0; i < valor - 1; i++) {
 }
 
 switch (orientacion) {
-  case "v":
-    if (direccion === "d") {
-      array.forEach((i) => console.log(i));
-    } else {
-      if (direccion === "i") {
-        for (let i = valor; i > 0; i--) {
-          console.log(array[i-1]);
-        }
-      }
+  case "vd":
+    console.log(`fibo<${valor}>: `)
+    array.forEach((i) => console.log(i));
+    break;
+  case "vi":
+    console.log(`fibo<${valor}>:`)
+    for (let i = valor; i > 0; i--) {
+      console.log(array[i - 1]);
     }
     break;
-  case "h":
-    if (direccion === "d") {
-      console.log(`fibo<${valor}>: `, array.join(" "));
-    } else {
-      if (direccion === "i") {
-        for (let i = valor; i >= 0; i--) {
-          array2.push(array[i]);
-        }
-        console.log(`fibo<${valor}>: `, array2.join(" "));
-      }
+  case "hd":
+    console.log(`fibo<${valor}>: `, array.join(" "));
+    break;
+  case "hi":
+    for (let i = valor; i >= 0; i--) {
+      array2.push(array[i]);
     }
+    console.log(`fibo<${valor}>: `, array2.join(" "));
     break;
   default:
+    console.log('Opción no válida')
     break;
 }
