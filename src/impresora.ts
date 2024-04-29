b8ea79f8a1ee1f02b35ae0b7ed9db8c2aeb1e9ce
mbuchniv <mbuchniv@w3itsolutions.net>
2024-04-29 18:07:19 -0300

    Ordenado impresora

--- src/impresora.ts
+++ src/impresora.ts
@@ -4,23 +4,27 @@ export class Impresora{
         let array2=[]
         let fibo=`fibo <${valor}>: `
         switch (orientacion) {
+            case "hd":
+            fibo+= array.join(" ");
+            break;
+
             case "vd":
             array.forEach((i) => fibo+=array[i]);
             break;
+
             case "vi":
             for (let i = valor; i > 0; i--) {
                 fibo+=array[i - 1];
             }
             break;
-            case "hd":
-            fibo+= array.join(" ");
-            break;
+            
             case "hi":
             for (let i = valor; i >= 0; i--) {
                 array2.push(array[i]);
             }
             fibo+=array2.join(" ");
             break;
+            
             default:
             console.log('Opción no válida');
             break;
